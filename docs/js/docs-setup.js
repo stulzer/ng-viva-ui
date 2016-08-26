fd6e67d1b3d87d8738eb3cef865b2bb0e62e4acf
Rafael Tavares <rafael.tavares@vivareal.com>
2016-08-26 11:24:43 -0300

    First docs release

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,102 @@
+NG_DOCS={
+  "sections": {
+    "api": "UI Kit"
+  },
+  "pages": [
+    {
+      "section": "api",
+      "id": "angularUtils.directives.dirPagination",
+      "shortName": "angularUtils.directives.dirPagination",
+      "type": "overview",
+      "moduleName": "angularUtils.directives.dirPagination",
+      "shortDescription": "angularUtils.directives.dirPagination",
+      "keywords": "angularutils api directives dirpagination overview"
+    },
+    {
+      "section": "api",
+      "id": "ngVivaUi",
+      "shortName": "ngVivaUi",
+      "type": "overview",
+      "moduleName": "ngVivaUi",
+      "shortDescription": "ngVivaUi",
+      "keywords": "angularjs api implementation kit ngvivaui overview ui viva"
+    },
+    {
+      "section": "api",
+      "id": "ngVivaUi.directive:vivaUiCheckbox",
+      "shortName": "vivaUiCheckbox",
+      "type": "directive",
+      "moduleName": "ngVivaUi",
+      "shortDescription": "UI Kit&#39;s checkbox implementation.",
+      "keywords": "api change checkbox directive enum implementation input kit ngvivaui tick type ui vivauicheckbox"
+    },
+    {
+      "section": "api",
+      "id": "ngVivaUi.directive:vivaUiFlatButton",
+      "shortName": "vivaUiFlatButton",
+      "type": "directive",
+      "moduleName": "ngVivaUi",
+      "shortDescription": "UI Kit&#39;s flat button implementation.",
+      "keywords": "api button default directive enum flat implementation kit large medium ngvivaui size ui vivauiflatbutton"
+    },
+    {
+      "section": "api",
+      "id": "ngVivaUi.directive:vivaUiIcon",
+      "shortName": "vivaUiIcon",
+      "type": "directive",
+      "moduleName": "ngVivaUi",
+      "shortDescription": "An iconset consumer.",
+      "keywords": "api consumer directive icon iconset ngvivaui vivauiicon"
+    },
+    {
+      "section": "api",
+      "id": "ngVivaUi.pagination",
+      "shortName": "ngVivaUi.pagination",
+      "type": "overview",
+      "moduleName": "ngVivaUi.pagination",
+      "shortDescription": "ngVivaUi.pagination",
+      "keywords": "angularjs angularutils api directives dirpagination implementation kit ngvivaui overview pagination ui viva"
+    },
+    {
+      "section": "api",
+      "id": "ngVivaUi.pagination.directive:vivaUiPaginationControls",
+      "shortName": "vivaUiPaginationControls",
+      "type": "directive",
+      "moduleName": "ngVivaUi.pagination",
+      "shortDescription": "UI Kit&#39;s pagination controls implementation.",
+      "keywords": "$scope api argument arguments callback clicked controller controls corresponding default defined dir-paginate dir-pagination-controls directive display equal exact group implementation instance instances integers kit left links lower maximum method minimum multiple mymethod navigated newpagenumber ngvivaui note number oldpagenumber on-page-change optional pagination passed setting time ui view vivauipaginationcontrols"
+    }
+  ],
+  "apis": {
+    "api": true
+  },
+  "__file": "_FAKE_DEST_/js/docs-setup.js",
+  "__options": {
+    "startPage": "/api",
+    "scripts": [
+      "js/angular.min.js",
+      "js/angular-animate.min.js",
+      "js/marked.js"
+    ],
+    "styles": [],
+    "title": "UI Kit",
+    "html5Mode": false,
+    "editExample": true,
+    "navTemplate": false,
+    "navContent": "",
+    "navTemplateData": {},
+    "loadDefaults": {
+      "angular": true,
+      "angularAnimate": true,
+      "marked": true
+    }
+  },
+  "html5Mode": false,
+  "editExample": true,
+  "startPage": "/api",
+  "scripts": [
+    "js/angular.min.js",
+    "js/angular-animate.min.js",
+    "js/marked.js"
+  ]
+};
